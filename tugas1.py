77801bd8b48052b5e9ba173961bfa456fc0a6e49
irtafaqohh <90293783+irtafaqohh@users.noreply.github.com>
2021-09-08 08:36:02 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,20 @@
+#Tugas 1 (Prakerja Baba Studio)
+#Nama : Leo Puji Christyanto
+#email : leopuji17@gmail.com
+
+nama = "leo"
+nilai = 80
+
+print(nama)
+print("Bernilai = ", nilai)
+
+if(nilai > 85):
+    print("Grade A")
+elif(nilai > 70):
+    print("Grade B")
+elif(nilai > 60):
+    print("Grade C")
+elif(nilai > 50):
+    print("Grade D")
+else:
+    print("Tidak Lulus")
